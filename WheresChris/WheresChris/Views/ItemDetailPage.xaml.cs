146a96b0d160cc86b2e36e99912cac7a7987db71
msneen <michael.sneen@gmail.com>
2017-04-11 10:00:58 -0700

    Add project files.

---
+++ WheresChris/WheresChris/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using WheresChris.ViewModels;
+
+using Xamarin.Forms;
+
+namespace WheresChris.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+		{
+			InitializeComponent();
+
+			BindingContext = this.viewModel = viewModel;
+		}
+	}
+}
