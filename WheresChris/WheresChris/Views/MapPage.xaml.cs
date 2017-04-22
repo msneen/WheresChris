61f1bad297daf116b82a432904eac087fcf27656
msneen <michael.sneen@gmail.com>
2017-04-22 12:32:25 -0700

    fixed Map Crash on Android

--- WheresChris/WheresChris/Views/MapPage.xaml.cs
+++ WheresChris/WheresChris/Views/MapPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace WheresChris.Views
@@ -14,7 +15,26 @@ namespace WheresChris.Views
 	{
 		public MapPage ()
 		{
-			InitializeComponent ();
-		}
+            InitializeComponent ();
+		    //try
+		    //{
+      //          var map = new Map(
+      //              MapSpan.FromCenterAndRadius(new Position(37, -122), Distance.FromMiles(0.3)))
+      //          {
+      //              IsShowingUser = true,
+      //              HeightRequest = 100,
+      //              WidthRequest = 960,
+      //              VerticalOptions = LayoutOptions.FillAndExpand
+      //          };
+      //          var stack = new StackLayout { Spacing = 0 };
+      //          stack.Children.Add(map);
+      //          Content = stack;
+      //      }
+      //      catch (Exception ex)
+		    //{
+		        
+		    //}
+
+        }
 	}
 }
