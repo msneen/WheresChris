92a50ddc5619db366a4bc17146cf00ba95c63cf2
msneen <michael.sneen@gmail.com>
2017-04-23 13:05:45 -0700

    iOs:  Added code for background location updates

--- WheresChris/WheresChris.iOS/AppDelegate.cs
+++ WheresChris/WheresChris.iOS/AppDelegate.cs
@@ -4,21 +4,26 @@ using Microsoft.Azure.Mobile;
 using Microsoft.Azure.Mobile.Analytics;
 using Microsoft.Azure.Mobile.Crashes;
 using UIKit;
+using WheresChris.iOS.Classes;
 
 namespace WheresChris.iOS
 {
 	[Register("AppDelegate")]
 	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
 	{
+	    public static LocationManager LocationManager = null;
+
 		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{
 			global::Xamarin.Forms.Forms.Init();
 			LoadApplication(new App());
 
-            MobileCenter.Start("2cd11ff1-c5b1-47d8-ac96-9fa5b74a47bd",
-                typeof(Analytics), typeof(Crashes));
+            MobileCenter.Start("2cd11ff1-c5b1-47d8-ac96-9fa5b74a47bd", typeof(Analytics), typeof(Crashes));
 
             Xamarin.FormsMaps.Init();
+
+            LocationManager = new LocationManager();
+            LocationManager.StartLocationUpdates();
 
             return base.FinishedLaunching(app, options);
 		}
---
+++ WheresChris/WheresChris.iOS/Classes/LocationManager.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Collections.Generic;
+using CoreLocation;
+using Plugin.Geolocator.Abstractions;
+using StayTogether;
+using StayTogether.Classes;
+using StayTogether.Group;
+using StayTogether.Location;
+using UIKit;
+
+namespace WheresChris.iOS.Classes
+{
+    public class LocationManager
+    {
+
+        CLLocationManager _clLocationManager;
+        private CLLocation _lastLocation;
+        private LocationSender _locationSender;
+        private SendMeter _sendMeter;
+        public string UserPhoneNumber { get; set; }
+
+        public LocationSender LocationSender => _locationSender;
+
+        public CLLocationManager ClLocationManager
+        {
+            get
+            {
+                return _clLocationManager;               
+            }
+            private set
+            {
+                _clLocationManager = value;
+            }
+        }
+
+        public LocationManager()
+        {
+            _sendMeter = new SendMeter(100, TimeSpan.FromMinutes(2));
+            _clLocationManager = new CLLocationManager
+            {
+                PausesLocationUpdatesAutomatically = false
+            };
+
+            // iOS 8 has additional permissions requirements
+            if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                _clLocationManager.RequestAlwaysAuthorization(); // works in background
+                
+            }
+
+            if (UIDevice.CurrentDevice.CheckSystemVersion(9, 0))
+            {
+                _clLocationManager.AllowsBackgroundLocationUpdates = true;
+            }
+
+        }
+
+        private void InitializeLocationSender()
+        {
+            _locationSender = new LocationSender();
+            _locationSender.InitializeSignalRAsync();
+        }
+
+        public void StartLocationUpdates()
+        {
+            if (CLLocationManager.LocationServicesEnabled)
+            {
+                //set the desired accuracy, in meters
+                _clLocationManager.DesiredAccuracy = 1;
+                _clLocationManager.LocationsUpdated += (object sender, CLLocationsUpdatedEventArgs e) =>
+                {
+                    // fire our custom Location Updated event
+                    if (e.Locations == null || e.Locations.Length <= -1) return;
+
+                    _lastLocation = e.Locations[e.Locations.Length - 1];
+                    //if more than 2 minutes or 100 feet from last location, send update to server
+                    SendPositionUpdate();
+                };
+
+                _clLocationManager?.StartUpdatingLocation();
+
+                if (UserPhoneNumber != null && UserPhoneNumber.Length > 10)
+                {
+                    InitializeLocationSender();
+                }
+            }
+        }
+
+        private void SendPositionUpdate()
+        {
+            var position = new Position
+            {
+                Latitude = _lastLocation.Coordinate.Latitude,
+                Longitude = _lastLocation.Coordinate.Longitude
+            };
+
+            if (UserPhoneNumber == null || !_sendMeter.CanSend(position)) return;
+
+            //Send position update
+            var groupMemberVm = new GroupMemberVm
+            {
+                //Get Group Member Properties
+                Name = "iPhoneTester",
+                PhoneNumber = UserPhoneNumber,
+                Latitude = _lastLocation.Coordinate.Latitude,
+                Longitude = _lastLocation.Coordinate.Longitude
+            };
+            _locationSender?.SendUpdatePosition(groupMemberVm);
+        }
+
+        public async void StartGroup(List<GroupMemberVm> selectedContacts)
+        {
+
+            if (_lastLocation != null)
+            {
+                var position = new Position
+                {
+                    Latitude = _lastLocation.Coordinate.Latitude,
+                    Longitude = _lastLocation.Coordinate.Longitude
+                };
+
+                var expireInHours = 6;
+
+                var groupVm = GroupHelper.InitializeGroupVm(selectedContacts, position, UserPhoneNumber, expireInHours);
+
+                if (_locationSender == null)
+                {
+                    InitializeLocationSender();
+                }
+                await _locationSender.StartGroup(groupVm);
+            }
+
+        }
+    }
+}
