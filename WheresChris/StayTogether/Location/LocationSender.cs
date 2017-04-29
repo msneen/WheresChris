97415ea0caf1de5205eb81afdaa634623b34b01c
msneen <michael.sneen@gmail.com>
2017-04-29 00:28:09 -0700

    changed connection to azure with https

--- WheresChris/StayTogether/Location/LocationSender.cs
+++ WheresChris/StayTogether/Location/LocationSender.cs
@@ -42,7 +42,7 @@ namespace StayTogether
 	    public void InitializeSignalRAsync()
         {
             // Connect to the server
-            _hubConnection = new HubConnection("http://staytogetherserver.azurewebsites.net/");//mike
+            _hubConnection = new HubConnection("https://staytogetherserver.azurewebsites.net/");//mike
             //_hubConnection = new HubConnection("http://162.231.59.41/StayTogetherServer/");//mike
             _hubConnection.Headers.Add("AuthToken", "x0y2!tJyHR%$Sip@*%amaGxvs");
 
