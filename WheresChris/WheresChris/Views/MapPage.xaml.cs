931be35ea5c1db74394ed79fa08acfa9378bf47c
msneen <michael.sneen@gmail.com>
2017-04-30 20:51:19 -0700

    iPhone:  fixed map location bug

--- WheresChris/WheresChris/Views/MapPage.xaml.cs
+++ WheresChris/WheresChris/Views/MapPage.xaml.cs
@@ -3,19 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Plugin.Geolocator;
-using StayTogether;
 using StayTogether.Classes;
 using WheresChris.Helpers;
 using WheresChris.Views.GroupViews;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
-//#if __ANDROID__
-//using StayTogether.Droid.Services;
-//#endif
-//#if __IOS__
-//using WheresChris.iOS;
-//#endif
 
 namespace WheresChris.Views
 {
@@ -29,30 +22,24 @@ namespace WheresChris.Views
 
 	    protected override async void OnAppearing()
 	    {
-            InitializeMap();
+            await InitializeMap();
         }
 
 
-	    private async void InitializeMap()
+	    private async Task InitializeMap()
 	    {
-            try
-            {
-#if __ANDROID__
-                var mapPosition = await GetMapPosition();
+	        var mapPosition = await GetMapPosition();
 
-                GroupMap.MoveToRegion(
-                    MapSpan.FromCenterAndRadius(
-                        mapPosition, Distance.FromMiles(1)));
-                UpdateMap();
-#endif
-            }
-            catch (Exception ex) { }
-
+	        GroupMap.MoveToRegion(
+	            MapSpan.FromCenterAndRadius(
+	                mapPosition, Distance.FromMiles(1)));
+	        UpdateMap();
 	    }
 
 	    private static async Task<Position> GetMapPosition()
 	    {
-	        var userPosition = await CrossGeolocator.Current.GetLastKnownLocationAsync();
+	        CrossGeolocator.Current.DesiredAccuracy = 50;
+            var userPosition = await CrossGeolocator.Current.GetPositionAsync(new TimeSpan(0,0,10));
             
 	        if (userPosition == null) return new Position(32.7157, -117.1611);
 
@@ -66,12 +53,12 @@ namespace WheresChris.Views
 	        UpdateMap(groupMembers);
 	    }
 
-
-
 	    private void UpdateMap(List<GroupMemberVm> groupMembers)
 	    {
-            GroupMap.Pins.Clear();
-            foreach (var groupMember in groupMembers)
+	        if (groupMembers.Count <= 0) return;
+
+	        GroupMap.Pins.Clear();
+	        foreach (var groupMember in groupMembers)
 	        {
 	            var position = new Position(groupMember.Latitude, groupMember.Longitude);
 	            var pin = new Pin
@@ -113,12 +100,3 @@ namespace WheresChris.Views
 	    }
 	}
 }
-//#if __ANDROID__
-//	        userPosition = LocationSenderService.Instance.GetPosition();
-//#endif
-//#if __IOS__
-//	        if (AppDelegate.LocationManager != null && AppDelegate.LocationManager.ClLocationManager != null)
-//	        {
-//	            userPosition = AppDelegate.LocationManager.GetPosition();
-//	        }
-//#endif
