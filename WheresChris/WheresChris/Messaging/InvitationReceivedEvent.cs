fd33ca5c99bed5a537f2b536ccdac864c95d4bbf
msneen <michael.sneen@gmail.com>
2017-05-05 09:56:16 -0700

    Join page autoRefresh: 144940197: Working and tested

---
+++ WheresChris/WheresChris/Messaging/InvitationReceivedEvent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StayTogether;
+using StayTogether.Helpers;
+using Xamarin.Forms;
+using EventHandler = System.EventHandler;
+
+namespace WheresChris.Messaging
+{
+    public class InvitationReceivedEvent
+    {
+        public event EventHandler OnInvitationReceivedMsg;
+
+        private readonly TimeGate _invitationTimeGate= new TimeGate(1000);
+
+        public InvitationReceivedEvent(TimeSpan? interval = null)
+        {
+            if (interval.HasValue)
+            {
+                _invitationTimeGate = new TimeGate(interval.Value);
+            }
+
+            MessagingCenter.Subscribe<LocationSender>(this, LocationSender.GroupInvitationReceivedMsg,
+            (sender) =>
+            {
+                if (_invitationTimeGate.CanProcess(true))
+                {
+                    OnInvitationReceivedMsg?.Invoke(this, EventArgs.Empty);
+                }
+            });
+        }
+    }
+}
