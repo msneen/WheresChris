8c0aa689576b15c55d081bbd320d77704bd97ef1
Michael Sneen <michael.sneen@gmail.com>
2018-04-15 14:46:26 -0700

    Added Authy.Net to stay together

---
+++ WheresChris/StayTogether/Authy.Net/RegisterUserResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Authy.Net
+{
+    /// <summary>
+    /// The result of a request to register a user
+    /// </summary>
+    public class RegisterUserResult : AuthyResult
+    {
+        /// <summary>
+        /// The user id of a succesful registration event
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// The user information on Authy API
+        /// </summary>
+        public Dictionary<string, string> User { get; set;}
+    }
+}
